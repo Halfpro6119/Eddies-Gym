fc198ed38667832b9a317a1f5a873522ca3f570f
rileywebboost-afk <rileywebboost@gmail.com>
2025-12-22 10:12:10 +0000

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -99,7 +99,7 @@ Sunday: 10:30 AM - 11:30 AM<br />
         </div>
 
         <div className="border-t border-neutral-800 mt-8 pt-8 text-center text-neutral-400 text-sm">
-          <p>&copy; 2025 Eddie's Gym. All rights reserved.</p>
+          <p>&copy; 2025 Eddie's Gym. All rights reserved.<p>
         </p>
         </div>
       </div>
